3ce369bd96e0a83fec6ca63a2506a70c98eb0e92
Jose Soto <josesoto1993@gmail.com>
2024-07-30 01:20:45 -0300

    base train troops loop

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,8 +2,11 @@
 const TRAVIAN_BASE = "https://ts100.x10.asia.travian.com/dorf1.php";
 const TRAVIAN_FARM_LIST =
   "https://ts100.x10.asia.travian.com/build.php?id=39&gid=16&tt=99";
+const TARVIAN_MAIN_BARRACKS =
+  "https://ts100.x10.asia.travian.com/build.php?newdid=19418&id=22&gid=19";
 
 module.exports = {
   TRAVIAN_BASE,
   TRAVIAN_FARM_LIST,
+  TARVIAN_MAIN_BARRACKS,
 };
