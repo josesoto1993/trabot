6e529151818c47e73a89789729b93500a1301107
Jose Soto <josesoto1993@gmail.com>
2024-07-30 14:03:21 -0300

    more exact attack (5'+-30")

--- src/services/attackFarmsService.js
+++ src/services/attackFarmsService.js
@@ -4,8 +4,8 @@ const { TRAVIAN_FARM_LIST } = require("../config/constants");
 let lastAttackTime = 0;
 let randomInterval = 0;
 let attackCount = 0;
-const MIN_ATTACK_INTERVAL = 4 * 60 * 1000;
-const RANDOM_INTERVAL_VARIATION = 2 * 60 * 1000;
+const MIN_ATTACK_INTERVAL = 5 * 60 * 1000;
+const RANDOM_INTERVAL_VARIATION = 0.5 * 60 * 1000;
 const MIN_CLICK_INTERVAL = 3 * 1000;
 const MAX_CLICK_INTERVAL = 5 * 1000;
 
@@ -24,7 +24,9 @@ const hasEnoughTimePassed = (currentTime) => {
 };
 
 const updateNextAttackTime = () => {
-  randomInterval = Math.floor(Math.random() * RANDOM_INTERVAL_VARIATION);
+  randomInterval = Math.floor(
+    Math.random() * RANDOM_INTERVAL_VARIATION * 2 - RANDOM_INTERVAL_VARIATION
+  );
   lastAttackTime = Date.now();
   attackCount = attackCount + 1;
 };
