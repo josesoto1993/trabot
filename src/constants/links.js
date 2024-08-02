38d476ad3735d34778c749df1eb8f698f98c6dd3
Jose Soto <josesoto1993@gmail.com>
2024-08-01 22:02:44 -0300

    add hero adventure service

--- src/constants/links.js
+++ src/constants/links.js
@@ -3,9 +3,12 @@ const TRAVIAN_FARM_LIST =
   "https://ts100.x10.asia.travian.com/build.php?id=39&gid=16&tt=99";
 const TARVIAN_MAIN_BARRACKS =
   "https://ts100.x10.asia.travian.com/build.php?newdid=19418&id=22&gid=19";
+const TRAVIAN_HERO_ADVENTURES =
+  "https://ts100.x10.asia.travian.com/hero/adventures";
 
 module.exports = {
   TRAVIAN_BASE,
   TRAVIAN_FARM_LIST,
   TARVIAN_MAIN_BARRACKS,
+  TRAVIAN_HERO_ADVENTURES,
 };
