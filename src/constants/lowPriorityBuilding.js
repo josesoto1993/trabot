576b1730ce0e11187892aeeaafc19e8f8fbcb81d
Jose Soto <josesoto1993@gmail.com>
2024-08-13 16:58:57 -0300

    add palilsade as low priority

--- src/constants/lowPriorityBuilding.js
+++ src/constants/lowPriorityBuilding.js
@@ -10,6 +10,7 @@ const LowPriorityBuildings = [
   { type: BuildingTypes["Stonemason's Lodge"], level: 20 },
   { type: BuildingTypes["Palace"], level: 20 },
   { type: BuildingTypes["Rally Point"], level: 10 },
+  { type: BuildingTypes["Palisade"], level: 10 },
 ];
 
 module.exports = LowPriorityBuildings;
