03389c8a4a0e1c83b2be441413e30d30d9ba0fbc
Jose Soto <josesoto1993@gmail.com>
2024-08-13 16:56:10 -0300

    update skips

--- src/constants/skipBuild.js
+++ src/constants/skipBuild.js
@@ -1,4 +1,18 @@
-const SkipCreation = ["HDS 01", "HDS 02"];
-const SkipUpgrade = ["HDS 01", "HDS 02"];
+const SkipCreation = [
+  "HDS 01",
+  "HDS 02",
+  "HDS 03",
+  "HDS 04",
+  "HDS 05",
+  "HDS 06",
+  "HDS 08",
+  "HDS 09",
+  "HDS 10",
+  "HDS 11",
+  "HDS 12",
+  "HDS 13",
+  "HDS 14",
+];
+const SkipUpgrade = [];
 
 module.exports = { SkipCreation, SkipUpgrade };
