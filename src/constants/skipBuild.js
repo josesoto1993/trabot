2a4aa325aab23c827d010150c08bb012c2f86421
Jose Soto <josesoto1993@gmail.com>
2024-08-13 22:00:33 -0300

    skip creation only in capital

--- src/constants/skipBuild.js
+++ src/constants/skipBuild.js
@@ -1,18 +1,4 @@
-const SkipCreation = [
-  "HDS 01",
-  "HDS 02",
-  "HDS 03",
-  "HDS 04",
-  "HDS 05",
-  "HDS 06",
-  "HDS 08",
-  "HDS 09",
-  "HDS 10",
-  "HDS 11",
-  "HDS 12",
-  "HDS 13",
-  "HDS 14",
-];
+const SkipCreation = ["HDS 01"];
 const SkipUpgrade = [];
 
 module.exports = { SkipCreation, SkipUpgrade };
