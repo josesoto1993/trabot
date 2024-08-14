6315296e64f8eaa5633419e8edfd37dee12f133f
Jose Soto <josesoto1993@gmail.com>
2024-08-13 22:25:42 -0300

    unit and train list constants

---
+++ src/constants/trainList.js
@@ -0,0 +1,42 @@
+const Unit = require("../constants/units");
+
+const TrainList = [
+  {
+    villageName: "HDS 01",
+    unit: Unit.Swordsman,
+  },
+  {
+    villageName: "HDS 01",
+    unit: Unit.Haeduan,
+  },
+  {
+    villageName: "HDS 02",
+    unit: Unit.Phalanx,
+  },
+  {
+    villageName: "HDS 03",
+    unit: Unit.Phalanx,
+  },
+  {
+    villageName: "HDS 04",
+    unit: Unit.Phalanx,
+  },
+  {
+    villageName: "HDS 05",
+    unit: Unit.Phalanx,
+  },
+  {
+    villageName: "HDS 06",
+    unit: Unit.Phalanx,
+  },
+  {
+    villageName: "HDS 07",
+    unit: Unit.Phalanx,
+  },
+  {
+    villageName: "HDS 08",
+    unit: Unit.Phalanx,
+  },
+];
+
+module.exports = TrainList;
--- src/constants/units.js
+++ src/constants/units.js
@@ -1,14 +1,16 @@
+const BuildingTypes = require("../constants/buildingTypes");
+
 const Unit = {
-  Phalanx: "t1",
-  Swordsman: "t2",
-  Pathfinder: "t3",
-  TheutatesThunder: "t4",
-  Druidrider: "t5",
-  Haeduan: "t6",
-  Ram: "t7",
-  Trebuchet: "t8",
-  Chieftain: "t9",
-  Settler: "t10",
+  Phalanx: { selector: "t1", building: BuildingTypes.Barracks },
+  Swordsman: { selector: "t2", building: BuildingTypes.Barracks },
+  Pathfinder: { selector: "t3", building: BuildingTypes.Stable },
+  TheutatesThunder: { selector: "t4", building: BuildingTypes.Stable },
+  Druidrider: { selector: "t5", building: BuildingTypes.Stable },
+  Haeduan: { selector: "t6", building: BuildingTypes.Stable },
+  Ram: { selector: "t7", building: BuildingTypes.Workshop },
+  Trebuchet: { selector: "t8", building: BuildingTypes.Workshop },
+  Chieftain: { selector: "t9", building: BuildingTypes.Residence },
+  Settler: { selector: "t10", building: BuildingTypes.Residence },
 };
 
 module.exports = Unit;
