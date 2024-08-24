a7d097029c9ce89889cb0faae1d72ed55c94b387
Jose Soto <josesoto1993@gmail.com>
2024-08-24 17:56:21 -0300

    new server

--- src/constants/troopsToUpgrade.js
+++ src/constants/troopsToUpgrade.js
@@ -1,30 +1,5 @@
 const Unit = require("./units");
 
-const UpgradeList = [
-  {
-    villageName: "HDS 20",
-    unit: Unit.TheutatesThunder,
-  },
-  {
-    villageName: "HDS 20",
-    unit: Unit.Haeduan,
-  },
-  {
-    villageName: "HDS 20",
-    unit: Unit.Ram,
-  },
-  {
-    villageName: "HDS 20",
-    unit: Unit.Trebuchet,
-  },
-  {
-    villageName: "HDS 35",
-    unit: Unit.Swordsman,
-  },
-  {
-    villageName: "HDS 35",
-    unit: Unit.Haeduan,
-  },
-];
+const UpgradeList = [];
 
 module.exports = UpgradeList;
